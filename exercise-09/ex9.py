0af2a41dc955c782dc8c47a2bc6028a3b59395f0
Eli Gundry <eligundry@gmail.com>
2013-06-06 13:16:36 -0400

    Completed exercise 9

---
+++ exercise-09/ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Due Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+With these three quotes
+We can type as many lines as you'd wish
+Literally
+All the lines you want
+"""
