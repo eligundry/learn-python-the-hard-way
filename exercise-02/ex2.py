03d66f3b085111f47638315c59139f2bffd207f3
Eli Gundry <eligundry@gmail.com>
2013-06-06 11:55:37 -0400

    Completed exercise 2"

---
+++ exercise-02/ex2.py
@@ -0,0 +1,8 @@
+# This is a comment
+
+print "I could have code like this." # and a comment afterwards
+
+# Comments are useful for disabling code
+
+print "This will run"
+# print "And this will not"
