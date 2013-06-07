def0c40297ba197cd3a1ea49199c249d77bc01db
Eli Gundry <eligundry@gmail.com>
2013-06-07 16:16:44 -0400

    Completed exercise 35

--- exercise-35/ex35.py
+++ exercise-35/ex35.py
@@ -37,5 +37,43 @@ How are you going to move the bear?
             dead("The bear gets pissed off and chews your leg off.")
         elif next == "open door" and bear_moved:
             gold_room()
+        else:
+            print "I got no idea what that means."
 
+def cthulhu_room():
+    print """
+Here you see the great evil, Cthulhu.
+He, it, whatever, stares at you and you go insane.
+Do you flee for your life or eat your head?
+    """
 
+    next = raw_input("> ")
+
+    if "flee" in next:
+        start()
+    elif "head" in next:
+        dead("Well, that was tasty!")
+    else:
+        cthulhu_room()
+
+def dead(why):
+    print "Good job!"
+    exit(0)
+
+def start():
+    print """
+You are in a dark room.
+There is a door to your right and left.
+Which do you take?
+    """
+
+    next = raw_input("> ")
+
+    if next == "left":
+        bear_room()
+    elif next == "right":
+        cthulhu_room()
+    else:
+        dead("You stumble around the room until you die.")
+
+start()
